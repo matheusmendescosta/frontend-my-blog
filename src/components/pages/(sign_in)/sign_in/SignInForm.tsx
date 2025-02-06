300b2c43b31cc1aa9b01357220382de171657bca
matheus mendes <matheus_mendess@live.com>
2025-02-06 11:51:31 -0300

    feat loading isSubmitting

--- src/components/pages/(sign_in)/sign_in/SignInForm.tsx
+++ src/components/pages/(sign_in)/sign_in/SignInForm.tsx
@@ -72,7 +72,18 @@ export const SignInForm = () => {
                     isSubmitting && 'cursor-not-allowed opacity-50'
                   )}
                 >
-                  Sign in
+                  {isSubmitting ? (
+                    <svg className="h-5 w-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                  ) : (
+                    'Sign in'
+                  )}
                 </button>
                 <a
                   href="/sign_up"
@@ -82,7 +93,18 @@ export const SignInForm = () => {
                     'bg-slate-600 hover:bg-slate-700 focus-visible:outline-indigo-600'
                   )}
                 >
-                  Sign up
+                  {isSubmitting ? (
+                    <svg className="h-5 w-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                  ) : (
+                    'Sign up'
+                  )}
                 </a>
               </div>
             </form>
